c7178a2bb88eceeb30258215720cc4df25c6b213
Ezra Newman <ezra@manmas.com>
2018-11-19 13:58:24 -0800

    restructure JS to its own file and fix firebase to use random ids

--- src/apiHandler.js
+++ src/apiHandler.js
@@ -10,20 +10,57 @@ firebase.initializeApp(config);
 
 var database = firebase.database();
 
-function getNumber() {
-    return firebase.database().ref('items/found/').limitToLast(1).once('value').then(function(snapshot) {
-        return Object.keys(snapshot.val())[0];
+
+function readFirebase() {
+    var jsonString;
+    database.ref('items/found').once('value', function (snap) {
+        firebaseIteratorTester(snap);
     });
 }
 
-function logArg(fromResolve) {
-    console.log(fromResolve);
+
+function firebaseIteratorTester(snap) {
+    let itemObj = snap.val();
+    let htmlTemplate = /*region HTMLCard*/ '<div class="col s12 m4">\n' +
+        '            <div class="card medium">\n' +
+        '                <div class="card-image">\n' +
+        '                    <img src="img-link-js-reserved">\n' +
+        '                    <span class="card-title">card-title-js-reserved</span>\n' +
+        '                </div>\n' +
+        '                <div class="card-content">\n' +
+        '                    <p>card-content-js-reserved</p>\n' +
+        '                </div>\n' +
+        '                <div class="card-action">\n' +
+        '                    <a href="#">View Comments</a>\n' +
+        '                    <a href="#">Report</a>\n' +
+        '                </div>\n' +
+        '            </div>\n' +
+        '        </div>'/*endregion*/;
+    let htmlOut = htmlTemplate.repeat(snap.numChildren());
+    for (let key in itemObj) {
+        let cardData = itemObj[key];
+        for (let target in cardData) {
+            htmlOut = htmlOut.replace(target + "-js-reserved", cardData[target])
+        }
+    }
+    document.getElementById("content").innerHTML = htmlOut;
 }
 
+
 function writeItemData(imgLink, cardTitle, cardContent) {
-    database.ref('items/found/2').set({
+    let randID = Math.random().toString(36).replace(/[^a-z]+/g, '');
+    while (checkIfKeyUsed(randID)){
+        randID = Math.random().toString(36).replace(/[^a-z]+/g, '');
+    }
+    database.ref('items/found/' + randID).set({
         "img-link": imgLink,
         "card-title": cardTitle,
-        "card-content" : cardContent
+        "card-content": cardContent
+    });
+}
+//returns true if key is in use
+function checkIfKeyUsed(key) {
+    database.ref('items/found/' + key).once('value', function (snap) {
+        return (snap.val() == null);
     });
 }
