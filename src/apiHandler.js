c62d5ebf1d5097f061917a9e67322e1b9f3cf705
Ezra Newman <ezra@manmas.com>
2018-11-19 15:19:33 -0800

    implement a destinction between lost and found items

--- src/apiHandler.js
+++ src/apiHandler.js
@@ -11,56 +11,65 @@ firebase.initializeApp(config);
 var database = firebase.database();
 
 
-function readFirebase() {
-    var jsonString;
-    database.ref('items/found').once('value', function (snap) {
-        firebaseIteratorTester(snap);
-    });
+function readFirebase(gettingLost) {
+    if (gettingLost) {
+        database.ref('items/found').once('value', function (snap) {
+            htmlInjector(snap, gettingLost);
+        });
+    } else {
+        database.ref('items/lost').once('value', function (snap) {
+            htmlInjector(snap);
+        });
+    }
 }
 
 
-function firebaseIteratorTester(snap) {
-    let itemObj = snap.val();
-    let htmlTemplate = /*region HTMLCard*/ '<div class="col s12 m4">\n' +
-        '            <div class="card medium">\n' +
-        '                <div class="card-image">\n' +
-        '                    <img src="img-link-js-reserved">\n' +
-        '                    <span class="card-title">card-title-js-reserved</span>\n' +
-        '                </div>\n' +
-        '                <div class="card-content">\n' +
-        '                    <p>card-content-js-reserved</p>\n' +
-        '                </div>\n' +
-        '                <div class="card-action">\n' +
-        '                    <a href="#">View Comments</a>\n' +
-        '                    <a href="#">Report</a>\n' +
-        '                </div>\n' +
-        '            </div>\n' +
-        '        </div>'/*endregion*/;
-    let htmlOut = htmlTemplate.repeat(snap.numChildren());
-    for (let key in itemObj) {
-        let cardData = itemObj[key];
-        for (let target in cardData) {
-            htmlOut = htmlOut.replace(target + "-js-reserved", cardData[target])
+function htmlInjector(snap, gettingLost) {
+    if (snap.numChildren() === 0) {
+        if (gettingLost)
+            htmlOut = "Sorry, no found items in the system right now.";
+        else
+            htmlOut = "Sorry, no lost items in the system right now.";
+    } else {
+        let itemObj = snap.val();
+        let htmlTemplate = /*region HTMLCard*/ '<div class="col s12 m4">\n' +
+            '            <div class="card medium">\n' +
+            '                <div class="card-image">\n' +
+            '                    <img src="img-link-js-reserved">\n' +
+            '                    <span class="card-title">card-title-js-reserved</span>\n' +
+            '                </div>\n' +
+            '                <div class="card-content">\n' +
+            '                    <p>card-content-js-reserved</p>\n' +
+            '                </div>\n' +
+            '                <div class="card-action">\n' +
+            '                    <a href="#">View Comments</a>\n' +
+            '                    <a href="#">Report</a>\n' +
+            '                </div>\n' +
+            '            </div>\n' +
+            '        </div>'/*endregion*/;
+        var htmlOut = htmlTemplate.repeat(snap.numChildren());
+        for (let key in itemObj) {
+            let cardData = itemObj[key];
+            for (let target in cardData) {
+                htmlOut = htmlOut.replace(target + "-js-reserved", cardData[target])
+            }
         }
     }
     document.getElementById("content").innerHTML = htmlOut;
 }
 
 
-function writeItemData(imgLink, cardTitle, cardContent) {
+function writeItemData(imgLink, cardTitle, cardContent, userFoundItem) {
     let randID = Math.random().toString(36).replace(/[^a-z]+/g, '');
-    while (checkIfKeyUsed(randID)){
-        randID = Math.random().toString(36).replace(/[^a-z]+/g, '');
-    }
-    database.ref('items/found/' + randID).set({
+    //rather than check if the key is used we just accept the very small odds that it is in use. Simplifies logic.
+    let path = "";
+    if (userFoundItem)
+        path = 'items/found/';
+    else
+        path = 'items/lost/';
+    database.ref(path + randID).set({
         "img-link": imgLink,
         "card-title": cardTitle,
         "card-content": cardContent
-    });
-}
-//returns true if key is in use
-function checkIfKeyUsed(key) {
-    database.ref('items/found/' + key).once('value', function (snap) {
-        return (snap.val() == null);
     });
 }
